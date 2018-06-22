7889d026599364582a6f1ccc518235b8290b8597
Kousic Ayyadevara <akousic@gmail.com>
2018-06-22 17:29:22 +0530

    Third Commit

--- hello.py
+++ hello.py
@@ -1,1 +1,16 @@
 print("Hello World!")
+x = 1
+if x == 1:
+    print("value of x is 1")
+myint = 5
+myfloat = 7.0
+mystring = "Hello"
+mystring2 = 'Kousic'
+print(myint)
+print(myfloat)
+print(mystring)
+print(mystring2)
+print(type(mystring))
+print(type(True))
+
+print("kousic"*4)
